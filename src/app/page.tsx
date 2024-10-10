793b84ece7703a2680c0a8d23323f21344f4c5fd
C-gyeltshen <02230279.cst@rub.edu.bt>
2024-10-10 16:03:55 +0600

    wip: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
 import Link from "next/link"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { cn } from "@/lib/utils"
+import Image from 'next/image'
+import pictur from "./images/building.jpg"
+
 
 import {
   NavigationMenu,
@@ -34,8 +37,8 @@ const components: { title: string; href: string; description: string }[] = [
 export default function Home() {
   return (
     
-    <div className="bg-red-200">
-      <div className="w-screen h-screen bg-yellow-100">
+    <div className="bg-white">
+      <div className="w-screen h-screen bg-white flow-root">
         <div className="w-screen h-24 bg-slate-400 flex flex-row">
           <div className="h-full w-1/6 bg-white flex items-center justify-center">
             <div className="w-20 h-20 	bg-red-400 rounded-full">
@@ -48,7 +51,7 @@ export default function Home() {
           <div className="h-full w-2/6 bg-white flex items-center justify-center">
             <h1>iBest Institute and Consultancy</h1>
           </div>
-          <div className="h-full w-3/6 bg-white align-middle flex items-center justify-center space-x-4">
+          <div className="h-full w-3/6 bg-white align-middle flex items-center justify-center space-x-4 float-right">
             <NavigationMenu>
               <NavigationMenuList>
                 <NavigationMenuItem>
@@ -92,16 +95,101 @@ export default function Home() {
             </NavigationMenu>
           </div>
         </div>
-      </div>
-      
-      <div className="w-screen h-screen bg-red-100 flex items-center justify-center">
-        <h1 className="text-2xl">page2</h1>
+        <div className="w-full h-2/4 bg-white bg-[url('./images/building.jpg')] backdrop-saturate-125 bg-cover">
+          <div className="w-full h-3/6 flex">
+            <Avatar className="w-36 h-236 border-current	m-auto">
+              <AvatarImage src="https://scontent.fpbh1-1.fna.fbcdn.net/v/t39.30808-1/327248969_3545587495677789_7126060281644009145_n.jpg?stp=dst-jpg_s480x480&_nc_cat=109&ccb=1-7&_nc_sid=f4b9fd&_nc_ohc=y6S4kvC5zfIQ7kNvgHzE-2B&_nc_ht=scontent.fpbh1-1.fna&_nc_gid=AWAcEURGlTurvQwTak5fKf6&oh=00_AYDzHNpv-wOB_P23qCPDvC9PKRM0F9hi4p1D7E610boDGQ&oe=670C1672" alt="@shadcn" />
+              <AvatarFallback>iBEST</AvatarFallback>
+            </Avatar>
+          </div>
+          <div className="w-full h-3/6 m-auto">
+              <h1 className="text-center text-4xl	 font-extrabold	text-white animate-slideLeft stoke-black">“To become an acclaimed institution </h1>
+              <h1 className="text-center text-3xl font-semibold text-white animate-slideRight	">and one-stop learning center in</h1> 
+              <h1 className="text-center text-3xl	font-semibold text-white animate-slideLeft"> Bhutan and beyond”</h1>  
+          </div>
+        </div>
+        <div className="w-full h-2/4">
+          <div className="w-full h-1/6 flex items-center justify-center">
+            <h1 className="text-2xl font-semibold">Certified Training Institute Registered with </h1>
+          </div>
+          <div className="w-full h-5/6 flex items-center justify-center space-x-10">
+            <Avatar className="w-48 h-48 border-current animate-slideLeft">
+              <AvatarImage src="https://www.bmhc.gov.bt/wp-content/uploads/2023/07/tvet.png" alt="@shadcn" />
+              <AvatarFallback>iBEST</AvatarFallback>
+            </Avatar>
+            <Avatar className="w-48 h-44 border-current	mb-36">
+              <AvatarImage src="https://www.bmf.bt/wp-content/uploads/2018/10/rgob-logo.png" alt="@shadcn" />
+              <AvatarFallback>iBEST</AvatarFallback>
+            </Avatar>
+            <Avatar className="w-48 h-44 border-current animate-slideRight	">
+              <AvatarImage src="https://vacancybt.b-cdn.net/wp-content/uploads/2019/02/www.health.gov_.bt_.png" alt="@shadcn" />
+              <AvatarFallback>iBEST</AvatarFallback>
+            </Avatar>
+          </div>
+        </div>
       </div>
 
-      <div className="w-screen h-screen bg-green-100 flex items-center justify-center">
-        <h1 className="text-2xl">page3</h1>
+      <div className="w-screen h-screen bg-white flex  flex-col items-center justify-center">
+        <div className="w-full h-1/6 flex items-center justify-center ">
+          <h1 className="text-3xl font-semibold text-blueDark ">MISSIONS</h1>
+        </div>
+        <div className="w-full h-1/6  flex justify-center">
+          <div className="w-2/5 h-1/2 bg-blue rounded flex items-center justify-center">
+            <h1 className="font-medium">Empower individual, community, and organization.</h1>
+          </div>
+        </div>
+        <div className="w-full h-1/6 flex justify-center">
+          <div className="w-2/5 h-1/2 bg-blue rounded flex items-center justify-center">
+            <h1 className="font-medium">Create enterprises and nurture entrepreneurs.</h1>
+          </div>
+        </div>
+        <div className="w-full h-1/6 flex justify-center">
+          <div className="w-2/5 h-1/2 bg-blue rounded flex items-center justify-center">
+            <h1 className="font-medium">Contribute towards employment generation.</h1>
+          </div>
+        </div>
+        <div className="w-full h-1/6 flex justify-center">
+          <div className="w-2/5 h-1/2 bg-blue rounded flex items-center justify-center">
+            <h1 className="font-medium">Develop and improve the rural economy.</h1>
+          </div>
+        </div>
+        <div className="w-full h-1/6 bg-blue-100 flex justify-center">
+          <div className="w-2/5 h-1/2 bg-blue rounded flex items-center justify-center">
+            <h1 className="font-medium">Foster knowledge creation and development.</h1>
+          </div>
+        </div>
       </div>
 
+      <div className="w-screen h-screen ">
+        <div className="w-full h-1/6 flex items-center justify-center">
+          <h1 className="text-3xl text-blueDark font-semibold">Our Expertise</h1>    
+        </div>
+        <div className="h-full w-full flex flex-row">
+          <div className="h-full w-1/5 "></div>
+          <div className="h-full w-3/5 ">
+            <div className="w-full h-1/4 flex flex-row space-x-24">
+              <div className="w-3/12 h-full bg-lightBlue rounded">
+                
+              </div>
+              <div className="w-3/12 h-full bg-lightBlue rounded"></div>
+              <div className="w-3/12 h-full bg-lightBlue rounded"></div>
+            </div>
+            <div className="w-full h-10 "></div>
+            <div className="w-full h-1/4 flex flex-row space-x-24">
+              <div className="w-3/12 h-full bg-lightBlue rounded"></div>
+              <div className="w-3/12 h-full bg-lightBlue rounded"></div>
+              <div className="w-3/12 h-full bg-lightBlue rounded"></div>
+            </div>
+            <div className="w-full h-10"></div>
+            <div className="w-full h-1/4 flex flex-row space-x-24">
+              <div className="w-3/12 h-full bg-lightBlue rounded"></div>
+              <div className="w-3/12 h-full bg-lightBlue rounded"></div>
+              <div className="w-3/12 h-full bg-lightBlue rounded"></div>
+            </div>
+          </div>
+          <div className="h-full w-1/5"></div>
+        </div>
+      </div>
     </div>
     
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,63 +1,84 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-  	extend: {
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
+	darkMode: ["class"],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+		keyframes: {
+			slideRight: {
+			"0%": { opacity: "0", marginLeft: "-600px" },
+			"100%": { opacity: "1", marginLeft: "0" },
+			},
+			slideLeft: {
+			"0%": { opacity: "0", marginRight: "-600px" },
+			"100%": { opacity: "1", marginRight: "0" },
+			},
+		},
+		animation: {
+			slideRight: "slideRight 1s ease-in-out",
+			slideLeft: "slideLeft 1s ease-in-out",
+		},
+		colors: {
+			blue :'#ecfeff',
+			blueDark : '#043853',
+			lightBlue : '#ddf3fd',			
+			background: "hsl(var(--background))",
+			foreground: "hsl(var(--foreground))",
+			card: {
+			DEFAULT: "hsl(var(--card))",
+			foreground: "hsl(var(--card-foreground))",
+			},
+			popover: {
+			DEFAULT: "hsl(var(--popover))",
+			foreground: "hsl(var(--popover-foreground))",
+			},
+			primary: {
+			DEFAULT: "hsl(var(--primary))",
+			foreground: "hsl(var(--primary-foreground))",
+			},
+			secondary: {
+			DEFAULT: "hsl(var(--secondary))",
+			foreground: "hsl(var(--secondary-foreground))",
+			},
+			muted: {
+			DEFAULT: "hsl(var(--muted))",
+			foreground: "hsl(var(--muted-foreground))",
+			},
+			accent: {
+			DEFAULT: "hsl(var(--accent))",
+			foreground: "hsl(var(--accent-foreground))",
+			},
+			destructive: {
+			DEFAULT: "hsl(var(--destructive))",
+			foreground: "hsl(var(--destructive-foreground))",
+			},
+			border: "hsl(var(--border))",
+			input: "hsl(var(--input))",
+			ring: "hsl(var(--ring))",
+			chart: {
+			1: "hsl(var(--chart-1))",
+			2: "hsl(var(--chart-2))",
+			3: "hsl(var(--chart-3))",
+			4: "hsl(var(--chart-4))",
+			5: "hsl(var(--chart-5))",
+			},
+		},
+		backgroundImage: {
+			homepage_image: "url('./images/building.jpg')", // Moved to the correct section
+		},
+		borderRadius: {
+			lg: "var(--radius)",
+			md: "calc(var(--radius) - 2px)",
+			sm: "calc(var(--radius) - 4px)",
+		},
+		},
+	},
+	plugins: [require("tailwindcss-animate")],
 };
+
 export default config;
